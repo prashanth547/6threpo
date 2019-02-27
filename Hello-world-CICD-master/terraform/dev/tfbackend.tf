7e47016c3c4957ebfbb2d9b5eba0e11e4bc3b954
prashanth547 <46973195+prashanth547@users.noreply.github.com>
2019-02-26 16:20:06 -0800

    Update tfbackend.tf

--- Hello-world-CICD-master/terraform/dev/tfbackend.tf
+++ Hello-world-CICD-master/terraform/dev/tfbackend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "bucket"
+    bucket  = "prashanthbucket547"
     key     = "dev.json"
-    region  = "us-west-2"
+    region  = "us-east-2"
     profile = "profile"
   }
 }
